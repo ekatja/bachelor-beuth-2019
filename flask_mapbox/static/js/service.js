1175ee7bf45ac43d5c83ab59c1bda8871b187dba
Ekaterina Anisimova <ekate.anisimova@gmail.com>
2019-01-18 17:34:54 +0100

    selected year can be saved to local storage

--- flask_mapbox/static/js/service.js
+++ flask_mapbox/static/js/service.js
@@ -1,14 +1,23 @@
+var loc, index;
+
 var year_select = document.getElementById("year-selector");
 var output = document.getElementById("demo");
-output.innerHTML = year_select.value; // Display the default slider value
-console.log(year_select.value);
-console.log(window.location);
-var loc;
-var path = window.location.pathname;
+
+if(localStorage.getItem('selected_year')) {
+  // Retrieve selected year from local storage
+  document.getElementById("year-selector").selectedIndex = localStorage.getItem("selected_year");
+  output.innerHTML = year_select.value;
+} else {
+  output.innerHTML = year_select.value; // Display the default slider value
+}
 
 // Update the current slider value (each time you drag the slider handle)
 year_select.oninput = function() {
+
   output.innerHTML = this.value;
+  index = year_select.selectedIndex;
+  // Store selected year in local storage
+  localStorage.setItem("selected_year", index);
 
   loc = window.location;
   loc.href = this.value;
